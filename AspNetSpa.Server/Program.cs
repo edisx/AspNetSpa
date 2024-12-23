45f0db5292c0b3efd9b1f73338642988231e5bca
Your Name <you@example.com>
2024-12-23 21:46:39 +0200

    Add simple logs

--- AspNetSpa.Server/Program.cs
+++ AspNetSpa.Server/Program.cs
@@ -1,11 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.ClearProviders();
+builder.Logging.AddSimpleConsole(options =>
+{
+    options.TimestampFormat = "[yyyy-MM-dd HH:mm:ss] ";
+    options.SingleLine = true;
+});
+
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.MapGet("/api", () => "hello mir");
+
+app.MapGet("/api/visitor", (ILogger<Program> logger, HttpContext context) =>
+{
+    var ip = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+    logger.LogInformation($"Visitor from IP: {ip}");
+
+    return Results.Ok(new { ip });
+});
 
 app.MapFallbackToFile("/index.html");
 
